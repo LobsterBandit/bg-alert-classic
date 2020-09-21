06c3c72f492b6274520f0010c8c751967073727b
lobsterbandit <code.lobsterbandit@outlook.com>
2020-09-21 21:56:39 +0000

    More descriptive components

--- client/cmd/classic-bg-ocr-tui/main.go
+++ client/cmd/classic-bg-ocr-tui/main.go
@@ -7,36 +7,38 @@ import (
 func main() {
 	app := tview.NewApplication()
 
-	topLeft := tview.NewTextView().SetText(`Current image of screen capure area`)
-	topLeft.Box.SetBorder(true).SetTitle("Top Left")
+	currentCapture := tview.NewTextView().SetText(`Current image of screen capure area.
+Cache image and retake every X seconds or when capture area changes.`)
+	currentCapture.Box.SetBorder(true).SetTitle("Latest Screen Capture")
 
 	captureOverlay := tview.NewTextView().
-		SetText("Show overlay of current capture area over total screen area")
+		SetText(`Show overlay of current capture area over total screen area.
+Simple wireframe display of two rectangles of different transparencies.`)
 	captureOverlay.Box.SetBorder(true).SetTitle("Capture Overlay")
 
 	captureAreaForm := tview.NewTextView().SetText("Form to set the capture area")
 	captureAreaForm.Box.SetBorder(true).SetTitle("Capture Area")
 
-	bottomLeft := tview.NewFlex().
+	captureArea := tview.NewFlex().
 		SetDirection(tview.FlexColumn).
 		AddItem(captureOverlay, 0, 1, false).
 		AddItem(captureAreaForm, 0, 1, false)
 
-	topRight := tview.NewTextView().SetText(`Last preprocessed image and parsed result`)
-	topRight.Box.SetBorder(true).SetTitle("Top Right")
+	ocrResults := tview.NewTextView().SetText(`Last preprocessed image and parsed result`)
+	ocrResults.Box.SetBorder(true).SetTitle("OCR Results")
 
-	bottomRight := tview.NewTextView().SetText(`Streaming commands and form to set command parameters`)
-	bottomRight.Box.SetBorder(true).SetTitle("Bottom Right")
+	commands := tview.NewTextView().SetText(`Streaming commands and form to set command parameters`)
+	commands.Box.SetBorder(true).SetTitle("Commands")
 
 	flex := tview.NewFlex().
 		AddItem(tview.NewFlex().
 			SetDirection(tview.FlexRow).
-			AddItem(topLeft, 0, 1, false).
-			AddItem(bottomLeft, 0, 1, false), 0, 1, false).
+			AddItem(currentCapture, 0, 1, false).
+			AddItem(captureArea, 0, 1, false), 0, 1, false).
 		AddItem(tview.NewFlex().
 			SetDirection(tview.FlexRow).
-			AddItem(topRight, 0, 1, false).
-			AddItem(bottomRight, 0, 1, false), 0, 1, false)
+			AddItem(ocrResults, 0, 1, false).
+			AddItem(commands, 0, 1, false), 0, 1, false)
 
 	if err := app.SetRoot(flex, true).Run(); err != nil {
 		panic(err)
