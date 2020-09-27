523613a58bc41c9704916f6c46bc453cad20701b
LobsterBandit <code.lobsterbandit@outlook.com>
2020-09-27 04:35:52 +0000

    Add points flag to provide capture area

--- client/cmd/classic-bg-ocr/capture.go
+++ client/cmd/classic-bg-ocr/capture.go
@@ -4,6 +4,9 @@ import (
 	"errors"
 	"fmt"
 	"image"
+	"math"
+	"strconv"
+	"strings"
 
 	d "github.com/lobsterbandit/wowclassic-bg-ocr/client/pkg/discord"
 	"github.com/lobsterbandit/wowclassic-bg-ocr/client/pkg/img"
@@ -12,6 +15,8 @@ import (
 )
 
 var (
+	// points defining the capture rectangle
+	points []string
 	// send image for OCR analysis
 	analyze bool
 	// OCR analysis endpoint
@@ -36,11 +41,22 @@ var (
 		RunE:  runCapture,
 	}
 
+	ErrCaptureNotTwoPoints        = errors.New("capture requires exactly 2 points")
+	ErrCapturePointMalformed      = errors.New("point value malformed, expecting x,y format")
 	ErrCaptureIncompatibleFlagset = errors.New("incompatible set of flags")
 	ErrWebhookMissingRequiredArgs = errors.New("webhookID and webhookToken are required")
 )
 
 func init() {
+	addCaptureFlags()
+
+	rootCmd.AddCommand(captureCmd)
+}
+
+func addCaptureFlags() {
+	captureCmd.Flags().StringArrayVarP(
+		&points, "point", "p", make([]string, 0, 2), "a point in X,Y format; 2 points define the capture rectangle")
+
 	captureCmd.Flags().BoolVarP(&analyze, "analyze", "a", false, "analyze image for bg timers")
 	captureCmd.Flags().StringVarP(&ocrURL, "url", "u", "", "remote ocr analysis endpoint, required if analyze is set")
 
@@ -53,34 +69,20 @@ func init() {
 	captureCmd.Flags().StringVarP(&outFile, "out", "o", "", "path to write captured image")
 
 	captureCmd.Flags().StringVarP(&file, "file", "f", "", "path to a local image file")
-
-	rootCmd.AddCommand(captureCmd)
 }
 
 func runCapture(cmd *cobra.Command, args []string) (err error) {
 	// exit early if required arg combinations are not met
-	if discord && (webhookID == "" || webhookToken == "") {
-		return fmt.Errorf("missing required arguments to send discord webhooks: %w", ErrWebhookMissingRequiredArgs)
-	}
-
-	if analyze && ocrURL == "" {
-		return fmt.Errorf("url is required if analyze is set: %w", ErrCaptureIncompatibleFlagset)
+	err = validateFlags()
+	if err != nil {
+		return err
 	}
 
 	var imageFile *img.File
 	if file != "" {
 		imageFile, err = img.FromFile(file)
 	} else {
-		// TODO: get points from flags
-		pt0 := image.Point{
-			X: 575,
-			Y: 1275,
-		}
-		pt1 := image.Point{
-			X: 800,
-			Y: 1400,
-		}
-		imageFile, err = screen.Capture(image.Rectangle{pt0, pt1})
+		imageFile, err = captureScreen(points)
 	}
 
 	var results []img.BgTimer
@@ -104,7 +106,6 @@ func runCapture(cmd *cobra.Command, args []string) (err error) {
 			},
 		}
 
-		// webhook to post discord channel message
 		err = webhook.PostDiscordMessage()
 		if err != nil {
 			return err
@@ -113,10 +114,8 @@ func runCapture(cmd *cobra.Command, args []string) (err error) {
 
 	if write {
 		if outFile != "" {
-			// write file to given path
 			err = imageFile.Write(outFile)
 		} else {
-			// save to current directory
 			err = imageFile.Save()
 		}
 	}
@@ -124,4 +123,69 @@ func runCapture(cmd *cobra.Command, args []string) (err error) {
 	fmt.Println("\nComplete!")
 
 	return err
+}
+
+func validateFlags() error {
+	if n := len(points); n != 0 && n != 2 {
+		return fmt.Errorf("%d points provided: %w", n, ErrCaptureNotTwoPoints)
+	}
+
+	if discord && (webhookID == "" || webhookToken == "") {
+		return fmt.Errorf("missing required arguments to send discord webhooks: %w", ErrWebhookMissingRequiredArgs)
+	}
+
+	if analyze && ocrURL == "" {
+		return fmt.Errorf("url is required if analyze is set: %w", ErrCaptureIncompatibleFlagset)
+	}
+
+	return nil
+}
+
+func captureScreen(points []string) (imageFile *img.File, err error) {
+	if len(points) == 0 {
+		return screen.CapturePrimary()
+	}
+
+	min := image.Point{X: math.MaxInt32, Y: math.MaxInt32}
+	max := image.Point{X: 0, Y: 0}
+
+	for _, point := range points {
+		s := strings.Split(point, ",")
+
+		// nolint:gomnd
+		if len(s) != 2 {
+			return nil, fmt.Errorf("error parsing points: %w", ErrCapturePointMalformed)
+		}
+
+		x, err := strconv.Atoi(s[0])
+		if err != nil {
+			return nil, fmt.Errorf("error parsing X coord %v: %w", s[0], ErrCapturePointMalformed)
+		}
+
+		y, err := strconv.Atoi(s[1])
+		if err != nil {
+			return nil, fmt.Errorf("error parsing Y coord %v: %w", s[1], ErrCapturePointMalformed)
+		}
+
+		// ensure min and max arranged for rectangle creation
+		if x < min.X {
+			min.X = x
+		}
+
+		if x > max.X {
+			max.X = x
+		}
+
+		if y < min.Y {
+			min.Y = y
+		}
+
+		if y > max.Y {
+			max.Y = y
+		}
+	}
+
+	fmt.Printf("Capture Points\nMin: %v\nMax: %v\n\n", min, max)
+
+	return screen.Capture(image.Rectangle{min, max})
 }
